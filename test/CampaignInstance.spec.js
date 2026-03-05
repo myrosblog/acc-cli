0cea45151e897c671d7863af37e250eae4e6cd22
Myros <social.prevalent026@slmails.com>
2026-03-04 20:26:41 -0500

    + unit test CampaignInstance.parse

--- test/CampaignInstance.spec.js
+++ test/CampaignInstance.spec.js
@@ -17,7 +17,9 @@ const loadXml = (file) => DomUtil.parse(fs.readFileSync(configPathXml + file));
 // mocks
 const configDefault = loadJson("acc.config.defaultTemplate.json");
 const configDefaultNoMeta = loadJson("acc.config.defaultTemplateNoMeta.json");
-const xtkSqlPostgre = loadXml("xtk/sql/postgresql-tmpl-createdb.sql.xml");
+const xtkSqlCreatedb = loadXml("xtk/sql/createdb.sql.xml");
+const xtkSchemaDelivery = loadXml("xtk/srcSchema/nms-delivery.xml");
+const nmsDelivery554 = loadXml("nms/delivery/DM554.xml");
 // acc
 import CampaignInstance from "../src/CampaignInstance.js";
 import CampaignError from "../src/CampaignError.js";
@@ -57,15 +59,113 @@ describe("CampaignInstance", function () {
   });
 
   describe("parse", function () {
-    it("should parse XML xtk:sql without meta", async function () {
+    it("should parse xtk:sql without meta", async function () {
       instance = new CampaignInstance(mockClient, configDefaultNoMeta);
-      const child = DomUtil.getFirstChildElement(xtkSqlPostgre);
+      const child = DomUtil.getFirstChildElement(xtkSqlCreatedb);
       const schemaConfig = configDefaultNoMeta["xtk:sql"];
-      instance.parse(child, schemaConfig, join(__dirname, "../dist/"), "xtk:sql");
+      instance.parse(
+        child,
+        schemaConfig,
+        join(__dirname, "../dist/"),
+        "xtk:sql",
+      );
 
-      const filePath = join(__dirname, '../dist/Administration/Configuration/SQL scripts/xtk/postgresql-tmpl-createdb.sql.sql');
-      const fileExists = await fs.pathExists(filePath);
+      const fileRaw = join(
+        __dirname,
+        "../dist/Administration/Configuration/SQL scripts/xtk/createdb.sql.sql",
+      );
+      const fileExists = await fs.pathExists(fileRaw);
       expect(fileExists).to.be.true;
+      const content = fs.readFileSync(fileRaw, "utf8");
+      expect(content).to.contain(
+        `<sql xmlns="urn:xtk:queryDef" entitySchema="xtk:sql"`,
+      );
+      expect(content).to.contain(`md5="ABCDEF" name="createdb.sql"`);
+      expect(content).to.contain(`<data><![CDATA[-- comment here`);
+      expect(content).to.contain(`CREATE DATABASE $DatabaseName;`);
+      expect(content).to.contain(`]]></data></sql>`);
+    });
+
+    it("should parse xtk:sql with meta", async function () {
+      instance = new CampaignInstance(mockClient, configDefault);
+      const child = DomUtil.getFirstChildElement(xtkSqlCreatedb);
+      const schemaConfig = configDefault["xtk:sql"];
+      instance.parse(
+        child,
+        schemaConfig,
+        join(__dirname, "../dist/"),
+        "xtk:sql",
+      );
+
+      const fileSql = join(
+        __dirname,
+        "../dist/Administration/Configuration/SQL scripts/xtk/createdb.sql.sql",
+      );
+      const fileSqlExists = await fs.pathExists(fileSql);
+      expect(fileSqlExists).to.be.true;
+      const fileMeta = join(
+        __dirname,
+        "../dist/Administration/Configuration/SQL scripts/xtk/createdb.sql.meta.xml",
+      );
+      const fileMetaExists = await fs.pathExists(fileMeta);
+      expect(fileMetaExists).to.be.true;
+      const contentSql = fs.readFileSync(fileSql, "utf8");
+      const contentMeta = fs.readFileSync(fileMeta, "utf8");
+      expect(contentMeta).to.contain(
+        `<sql xmlns="urn:xtk:queryDef" entitySchema="xtk:sql"`,
+      );
+      expect(contentMeta).to.contain(`xtkschema="xtk:sql"/>`);
+      expect(contentMeta).to.not.contain(`<data`);
+      expect(contentSql).to.contain(`-- comment here`);
+      expect(contentSql).to.contain(`CREATE DATABASE $DatabaseName;`);
+      expect(contentSql).to.not.contain(`<`);
+      expect(contentSql).to.not.contain(`>`);
+    });
+
+    it("should parse nms:delivery without meta", async function () {
+      instance = new CampaignInstance(mockClient, configDefaultNoMeta);
+      const child = DomUtil.getFirstChildElement(nmsDelivery554);
+      const schemaConfig = configDefaultNoMeta["nms:delivery"];
+      instance.parse(
+        child,
+        schemaConfig,
+        join(__dirname, "../dist/"),
+        "nms:delivery",
+      );
+
+      const fileRaw = join(
+        __dirname,
+        "../dist/Campaign Management/Deliveries/DM554.html",
+      );
+      const fileExists = await fs.pathExists(fileRaw);
+      expect(fileExists).to.be.true;
+      const content = fs.readFileSync(fileRaw, "utf8");
+      expect(content).to.contain(`<delivery _operation="insert"`); // main element + attribute
+      expect(content).to.contain(`<folder _cs="`); // link
+      expect(content).to.contain(`<properties deliveryState="0"`); // element
+      expect(content).to.contain(`<content`); // no decomposition
+    });
+
+    it("should parse xtk:srcSchema without meta", async function () {
+      instance = new CampaignInstance(mockClient, configDefaultNoMeta);
+      const child = DomUtil.getFirstChildElement(xtkSchemaDelivery);
+      const schemaConfig = configDefaultNoMeta["xtk:srcSchema"];
+      instance.parse(
+        child,
+        schemaConfig,
+        join(__dirname, "../dist/"),
+        "xtk:srcSchema",
+      );
+
+      const fileRaw = join(
+        __dirname,
+        "../dist/Administration/Configuration/Data schemas/nms/delivery.xml",
+      );
+      const fileExists = await fs.pathExists(fileRaw);
+      expect(fileExists).to.be.true;
+      const content = fs.readFileSync(fileRaw, "utf8");
+      expect(content).to.contain(`<srcSchema name="delivery" namespace="nms"`); // main element + attribute
+      expect(content).to.contain(`<methods>`); // element
     });
   });
 });
---
+++ test/mocks/acc-js-sdk-xml/README.md
@@ -0,0 +1,1 @@
+Mocks from https://github.com/adobe/acc-js-sdk/blob/master/test/mock.js
