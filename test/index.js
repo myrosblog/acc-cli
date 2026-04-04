96f0b96cfebbda4bac45ab064fbe982d700b94d2
Myros <social.prevalent026@slmails.com>
2026-04-03 22:14:45 -0400

    test: added chaiAsPromised and suppressed AIO logs

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 import * as chai from "chai";
 import sinonChai from "sinon-chai";
+import chaiAsPromised from "chai-as-promised";
 
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 
 global.expect = chai.expect;
 
