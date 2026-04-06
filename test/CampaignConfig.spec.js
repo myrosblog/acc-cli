89ea93d177c2c8b89c1b8c33f66e3cd68c5cb575
Myros <social.prevalent026@slmails.com>
2026-04-06 17:46:22 -0400

    test: added CampaignConfig error codes

--- test/CampaignConfig.spec.js
+++ test/CampaignConfig.spec.js
@@ -7,6 +7,12 @@ import AioLogger from "@adobe/aio-lib-core-logging";
 const logger = AioLogger("CampaignAuth.spec");
 // acc
 import CampaignConfig from "../src/CampaignConfig.js";
+import { codes } from "../src/helpers/AccErrors.js";
+const {
+  CONFIG_CONSTR_DEFAULT_PATH_MISSING,
+  CONFIG_INIT_CONFIG_PATH_MISSING,
+  CONFIG_VALIDATE_ERRORS,
+} = codes;
 
 describe("CampaignAuth", function () {
   let tmpConfigPath;
@@ -23,27 +29,42 @@ describe("CampaignAuth", function () {
   });
 
   describe("CampaignConfig", () => {
+    describe("constructor", () => {
+      it("should throw on empty config path", () => {
+        expect(() => new CampaignConfig(logger)).to.throw(CONFIG_CONSTR_DEFAULT_PATH_MISSING);
+        expect(() => new CampaignConfig(logger, null)).to.throw(CONFIG_CONSTR_DEFAULT_PATH_MISSING);
+        expect(() => new CampaignConfig(logger, {})).to.throw(CONFIG_CONSTR_DEFAULT_PATH_MISSING);
+      });
+    });
+
     describe("init", () => {
-      it("should throw on empty config", () => {
-        expect(() => new CampaignConfig(logger)).to.throw();
-        expect(() => new CampaignConfig(logger, null)).to.throw();
-        expect(() => new CampaignConfig(logger, {})).to.throw();
+      it("should throw on empty config path", () => {
+        const config = new CampaignConfig(logger, tmpConfigPath);
+        expect(() => config.init()).to.throw(CONFIG_INIT_CONFIG_PATH_MISSING);
+        expect(() => config.init(null)).to.throw(CONFIG_INIT_CONFIG_PATH_MISSING);
+        expect(() => config.init("")).to.throw(CONFIG_INIT_CONFIG_PATH_MISSING);
       });
 
-      it("should create config from template if not existing", () => {
+      it("should create JSON config from template if not existing", () => {
         const config = new CampaignConfig(logger, tmpConfigPath);
         config.init(tmpConfigPath);
 
         const fileExists = fs.existsSync(tmpConfigPath);
         expect(fileExists).to.be.true;
+
+        const parsed = fs.readJsonSync(tmpConfigPath);
+
+        expect(parsed).to.have.property("schemas");
+        expect(parsed.schemas).to.be.an("array");
+
+        expect(parsed).to.have.property("acc-js-sdk");
+        expect(parsed["acc-js-sdk"]).to.be.an("object");
       });
     });
 
     describe("validate", () => {
       //   it("should validate the mock", () => {
-
       //   });
-
       // it("should validate the default config", () => {
       //   const config = new CampaignConfig(tmpConfigPath);
       //   config.init(tmpConfigPath);
